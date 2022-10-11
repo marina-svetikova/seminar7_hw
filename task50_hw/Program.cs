96858d9d3891775d4c683b91d26561ced05e3c3a
Svetikova Marina <m-golovleva@yandex.ru>
2022-10-12 00:13:34 +0500

    task52

---
+++ task50_hw/Program.cs
@@ -0,0 +1,68 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElement(int[,] matrix, int rows, int columns)
+{
+int newrows = matrix.GetLength(0);
+int newcolumns = matrix.GetLength(1); 
+    if (rows < newrows && columns < newcolumns)
+    {
+        Console.Write($"Элемент с индексами {rows},{columns} = {matrix[rows,columns]}");
+    }
+    else
+    {
+        Console.Write($"Элемента с индексами {rows},{columns} нет в массиве");
+    }
+}   
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n, 0, 10);
+PrintMatrix(matr);
+Console.WriteLine();
+
+Console.Write("Введите номер строки: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+FindElement(matr, rows, columns);
+
+
